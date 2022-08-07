27e376637ac1fc65e58e21031f2e4b7a6f8f713d
mingcheng.chua <mingcheng.chua@shopee.com>
2022-08-07 21:10:13 +0800

    added TableData to display Qualified Teams and Ranking of All Teams

--- champions-project/src/common/InputBox.js
+++ champions-project/src/common/InputBox.js
@@ -4,11 +4,19 @@ const InputBox = (props) => {
   const {labelTitle, text, handleChange} = props
 
   return (
-    <>
+    <div style={styles.labelTitleStyle}>
       <label>{labelTitle ? labelTitle : null}</label>
       <textarea onChange={(event) => handleChange(event)} value={text}></textarea>
-    </>
+    </div>
   ) 
+}
+
+const styles = {
+  labelTitleStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 }
 
 export default InputBox;
